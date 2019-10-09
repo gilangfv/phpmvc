7c91e13fcabaf8f7f52d815c44de7e75c13b59a0
gilangfv <gilangfauzan1@yahoo,com>
2019-10-09 11:05:35 +0700

    menambahkan template

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php 
 
-	class About{
+	class About extends Controller {
 
-		public function index($nama = "gilang", $pekerjaan = "player"){
-			echo "halo , saya $nama saya bekerja sebagai $pekerjaan";
+		public function index($nama = "gilang", $pekerjaan = "player", $umur = 20){
+			$data ['nama'] = $nama;
+			$data ['pekerjaan'] = $pekerjaan;
+			$data ['umur'] = $umur;
+			$data['judul']  = 'about me';
+			$this->view('templates/header', $data);
+			$this->view('about/index', $data);
+			$this->view('templates/footer');
 		}
 
 		public function page(){
-			echo "About/page";
+			$data['judul']  = 'my page';
+			$this->view('templates/header', $data);
+			$this->view('about/page');
+			$this->view('templates/footer');
 		}
 	}
 
---
+++ app/views/about/index.php
@@ -0,0 +1,4 @@
+
+<h1>halaman About</h1>
+
+<p>halo nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> dan pekerjaan saya <?= $data['pekerjaan']; ?></p>
