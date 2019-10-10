b862591f42abc76dc263a7a3cac9dbc4aa7a88b8
gilangfv <gilangfauzan1@yahoo,com>
2019-10-10 18:24:34 +0700

    menambahkan flah message

--- app/controllers/mahasiswa.php
+++ app/controllers/mahasiswa.php
@@ -20,7 +20,12 @@ class mahasiswa extends Controller {
 
 	public function tambah(){
 		 if ($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST)>0) {
-			 header('Location: ' . BASEURL . '/mahasiswa');
+			FLasher::setFlash('berhasil','ditambahkan','success');
+			header('Location: ' . BASEURL . '/mahasiswa');
+			 exit;
+		 } else{
+			FLasher::setFlash('gagal','ditambahkan','danger');
+			header('Location: ' . BASEURL . '/mahasiswa');
 			 exit;
 		 }
 	}
